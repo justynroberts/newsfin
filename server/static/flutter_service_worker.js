bebf3db35dab5b0cc05c597aa2e9f8ab424b98a5
Justyn Roberts <justynroberts@gmail.com>
2026-08-16 17:10:17 +0100

    Rebuild the web bundle
    New build stamp, so installed clients pick this up on their next launch.

--- server/static/flutter_service_worker.js
+++ server/static/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f70983812674a39a06b9a597494af036",
+const RESOURCES = {"flutter_bootstrap.js": "45bf3e1692913763366e576769e3f52a",
 "version.json": "37f5698b34fe7ecc165f0db66d55ab45",
-"index.html": "c3af65f5be3f9b3efd71cbd9e1912102",
-"/": "c3af65f5be3f9b3efd71cbd9e1912102",
+"index.html": "f0e931da459e177358e02cd201195ddb",
+"/": "f0e931da459e177358e02cd201195ddb",
 "main.dart.js": "e4595e567e7517c7aac68cfcc0054710",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "fcca998b20655f6be2fe309c5c1fdf44",
